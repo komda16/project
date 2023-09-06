1b61991c1c19765c647dd83d088bb83ea0bafe25
komda16 <140844362+komda16@users.noreply.github.com>
2023-09-06 22:06:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -638,8 +638,8 @@ def login():
     st.markdown(hide_st_style, unsafe_allow_html=True)
     global username
     with st.expander('login'):
-         username=st.sidebar.text_input('User Name')
-         password=st.sidebar.text_input('Password',type='password')
+         username=st.text_input('User Name')
+         password=st.text_input('Password',type='password')
     if st.checkbox('Login'):
 
         hashed_pswd=make_hashes(password)
